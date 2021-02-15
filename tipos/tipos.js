53a7cc743b832542071dc2049b0fb76e54ca21c6
Carlos Juliano <carlos_julianoti@outlook.com>
2021-02-15 14:30:52 -0300

    tipos string, number, boolen

---
+++ tipos/tipos.js
@@ -0,0 +1,17 @@
+"use strict";
+var nome = 'Beltrano';
+console.log(nome);
+// nome = 19
+var idade = 27;
+// idade = 'Ana';
+idade = 27.4;
+console.log(idade);
+// boolean
+var possuiobbies = false;
+// possuiobbies = 1;
+console.log(possuiobbies);
+// tipos explpicitos
+var minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = '27';
---
+++ tipos/tipos.ts
@@ -0,0 +1,19 @@
+let nome: string = 'Beltrano';
+console.log(nome);
+// nome = 19
+
+let idade: number = 27;
+// idade = 'Ana';
+idade = 27.4;
+console.log(idade);
+
+// boolean
+let possuiobbies: boolean = false;
+// possuiobbies = 1;
+console.log(possuiobbies);
+
+// tipos explpicitos
+let minhaIdade: number;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = '27';
