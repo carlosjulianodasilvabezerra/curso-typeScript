bec1473e335a09c57c13bfeb45286b177007052c
Carlos Juliano <carlos_julianoti@outlook.com>
2021-03-10 14:40:12 -0300

    finalizando about generics

--- generics/generics.ts
+++ generics/generics.ts
@@ -15,3 +15,33 @@ console.log(echoMelhorado('João').length);
 // console.log(echoMelhorado<number>(27).length);
 console.log(echoMelhorado<number>(27));
 console.log(echoMelhorado({ nome: 'João' }));
+
+// Desafio
+type Par<C, V> = { chave: C, valor: V}
+
+class Mapa<X, Y> {
+  itens: Array<Par<X, Y>> = new Array<Par<X, Y>>();
+
+  obter(chave: X): Par<X, Y> | null {
+    const resultado = this.itens.filter(i => i.chave === chave);
+
+    return resultado ? resultado[0] : null;
+  }
+
+  colocar(par: Par<X, Y>) {
+    const encontrado = this.obter(par.chave);
+    if(encontrado) {
+      encontrado.valor = par.valor;
+    } else {
+      this.itens.push(par)
+    }
+  }
+
+  limpar() {
+    this.itens = new Array<Par<X, Y>>()
+  }
+
+  imprimier() {
+    console.log(this.itens);
+  }
+}
